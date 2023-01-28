a77293e89716ada80dd9b6198b90652654a05fe6
badargondal <107700287+badargondal@users.noreply.github.com>
2023-01-28 05:34:34 +0500

    bids Portal

--- src/components/dashboard/BiddingReport.js
+++ src/components/dashboard/BiddingReport.js
@@ -13,27 +13,16 @@ function BiddingReport() {
       })
       .catch((e) => console.log(e));
   }, []);
-
-  const products = data?.map((i) => i.productId);
-  const unique = [...new Set(products?.map((item) => item._id))];
-  // console.log("arrayUniqueByKey", unique);
-
-  data?.sort((a, b) => (a.bidAmount > b.bidAmount ? -1 : 1));
-  console.log("data", data);
-
   return (
     <div className="container my-3">
       <table class="table table-hover thead-dark">
         <thead class="bg-dark">
           <tr class="text-light">
             <th scope="col">Product Id</th>
-            <th scope="col">Buyer Name</th>
             <th scope="col">Title</th>
-            <th scope="col">Price </th>
+            <th scope="col">Orignal Price </th>
+            <th scope="col">Bid Amount </th>
             <th scope="col">Buyer Name</th>
-            <th scope="col">Product Status</th>
-            <th scope="col">Number of Bids</th>
-            <th scope="col">Bid Winner Name</th>
             <th scope="col">Bid Winner Email</th>
           </tr>
         </thead>
@@ -42,14 +31,11 @@ function BiddingReport() {
             <>
               <tr>
                 <th scope="row">{item.productId._id}</th>
-                <td>{item.buyerId.name}</td>
-                <td>d.title</td>
+                <td>{item.productId.title}</td>
+                <td>{item.productId.price}</td>
                 <td>{item.bidAmount}</td>
-                <td>.name</td>
-                <td>d.status</td>
-                <td>asd</td>
-                <td>abc</td>
-                <td>abc@gmail.com</td>
+                <td>{item.buyerId.name}</td>
+                <td>{item.buyerId.email}</td>
               </tr>
             </>
           ))}
--- src/pages/buyer/products/PruchasedProducts.js
+++ src/pages/buyer/products/PruchasedProducts.js
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import React from "react";
+import Header from "../../../components/Header/header";
+import { useState } from "react";
+import getAuthorizedData from "../../../api/getAuthorizedData";
 function PruchasedProducts() {
+  const [data, setData] = useState(null);
+  React.useEffect(() => {
+    getAuthorizedData(`/admin/bids`)
+      .then((response) => {
+        console.log(response);
+        setData(response);
+      })
+      .catch((e) => console.log(e));
+  }, []);
   return (
-    <div>PruchasedProducts</div>
-  )
+    <>
+      <Header />
+      <div>PruchasedProducts</div>
+      <div className="container my-3">
+        <table class="table table-hover thead-dark">
+          <thead class="bg-dark">
+            <tr class="text-light">
+              <th scope="col">Product Id</th>
+              <th scope="col">Title</th>
+              <th scope="col">Orignal Price </th>
+              <th scope="col">Bid Amount </th>
+              <th scope="col">Buyer Name</th>
+              <th scope="col">Bid Winner Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map(
+              (item, index) =>
+                item.buyerId._id == localStorage.getItem("buyerId") && (
+                  <>
+                    <tr>
+                      <th scope="row">{item.productId._id}</th>
+                      <td>{item.productId.title}</td>
+                      <td>{item.productId.price}</td>
+                      <td>{item.bidAmount}</td>
+                      <td>{item.buyerId.name}</td>
+                      <td>{item.buyerId.email}</td>
+                    </tr>
+                  </>
+                )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
-export default PruchasedProducts
+export default PruchasedProducts;
